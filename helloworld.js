0500b33c4fadc29d4b76069334de57044c37b6df
Vladyslav Serdiuk <200406142@student.georgianc.on.ca>
2020-05-29 16:19:31 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Bye-bye World :C ');
+    res.end('Praise the Sun World ');
 });
 
 server.listen(port, hostname, () => {
